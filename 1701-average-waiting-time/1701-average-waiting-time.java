2250d622072f171c8fa196be49905b2781d5afa8
Santhosh : ) <117101431+Santhzsh@users.noreply.github.com>
2024-07-09 19:31:47 +0530

    Time: 4 ms (42.86%), Space: 73.2 MB (18.78%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.java
+++ 1701-average-waiting-time/1701-average-waiting-time.java
@@ -1,24 +1,20 @@
 class Solution {
     public double averageWaitingTime(int[][] customers) {
-       int totaltime= customers[0][0]+customers[0][1];
-       double Difference=Math.abs(totaltime-customers[0][0]);
-        for(int i=1;i<customers.length;i++)
-        {
-            if(customers[i][0]<totaltime)
-            {
-                totaltime+=customers[i][1];
-                Difference+=Math.abs(totaltime-customers[i][0]);
-            }
-            else
-            {
-                totaltime=customers[i][0]+customers[i][1];
-                Difference+=Math.abs(totaltime-customers[i][0]);
+        int totaltime = customers[0][0] + customers[0][1];
+        double Difference = Math.abs(totaltime - customers[0][0]);
+        for (int i = 1; i < customers.length; i++) {
+            if (customers[i][0] < totaltime) {
+                totaltime += customers[i][1];
+                Difference += Math.abs(totaltime - customers[i][0]);
+            } else {
+                totaltime = customers[i][0] + customers[i][1];
+                Difference += Math.abs(totaltime - customers[i][0]);
             }
 
         }
-  
-        double averagetime=Difference/customers.length;
+
+        double averagetime = Difference / customers.length;
         return averagetime;
-        
+
     }
 }
